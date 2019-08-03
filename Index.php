8b16d6a0d06065478ae436944cfb2ead4174b92f
iangelherrera <angelherrera505@yahoo.com>
2019-08-03 12:04:48 -0600

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,61 @@
+// Catch SQL Conection 
+
+
+
+<?php
+$serverName = "NTSERVER\sqlexpress"; //serverName\instanceName
+
+// Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
+// La conexión se intentará utilizando la autenticación Windows.
+
+$connectionInfo = array( "Database"=>"db_sgd");
+$conn = sqlsrv_connect( $db_sgd, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>S.G.D/title>
+  </head>
+  <body>
+  	<div class="row justify-content-center">
+    <form action="" method="POST">
+    	<div class="form-group">
+    	<label>Write ur Name</label>
+    	<input type="text" name="name" class="form-control" value="Enter UR Name">
+    	</div>
+    	<div class="form-group"><label>Write ur Location</label>
+    	<input type="text" name="loc" class="form-control" value="Enter ur tag">
+    	</div>
+    	<div class="form-group">
+    	<button type="submit" name="save"> Save </button>
+    	</div>
+    </form>
+    </div>
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
